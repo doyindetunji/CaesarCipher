7a3d6b2dea4ca9a3acc5c6511b4b1fe55bee52ad
Bearhost <adedoyin.adetunji@miva.edu.ng>
2025-01-20 19:38:01 +0100

    creating rainbow table generator in python

---
+++ RainbowTableGeneratorScript.py
@@ -0,0 +1,13 @@
+#Top 10 lists of common passwords in 2024 according to Nordpass
+passwords = ["123456", "123456789", "12345678", "password", "qwerty123", "qwerty1", "111111", "12345", "secret", "123123"]
+
+#Function to create md5 hash
+def md5sum(password):
+    return hashlib.md5(password.encode()).hexdigest()
+
+#function to create sha256 hash
+def sha256sum(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+#file to save the results
+OutputFile = "RainbowTable.csv"
